41c9d33113e78b51219bfa0b736b8edf67349855
Ethanb75 <belloramail@gmail.com>
2026-08-22 21:27:18 -0400

    fixing indents

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "proxmox" {
-    endpoint = var.proxmox_ve_endpoint
-    api_token = var.proxmox_ve_api_token
-    insecure = var.proxmox_ve_insecure
+  endpoint  = var.proxmox_ve_endpoint
+  api_token = var.proxmox_ve_api_token
+  insecure  = var.proxmox_ve_insecure
 }
