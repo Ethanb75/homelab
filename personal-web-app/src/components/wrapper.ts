16cc06450e32e9f76a4cd3311f35fc479eb254c6
Ethanb75 <belloramail@gmail.com>
2026-08-31 22:33:11 -0400

    adding src files

---
+++ personal-web-app/src/components/wrapper.ts
@@ -0,0 +1,51 @@
+import { LitElement, css, html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+
+const WrapperStyles = css`
+  :host {
+    display: block;
+  }
+`
+
+/**
+ * keep these on every page, but make them optional to show/hide
+ * 
+ * @slot - This element has a slot
+ */
+@customElement('wrapper')
+export class Wrapper extends LitElement {
+  /**
+   * The number of times the button has been clicked.
+   */
+  @property({ type: Number })
+  count = 0
+  static styles = [WrapperStyles]
+
+  render() {
+    return html`
+      <div>
+        <header>
+          <nav>
+            <ul>
+              <li><a href="/">Home</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/contact">Contact</a></li>
+            </ul>
+          </nav>
+        </header>
+        <main>
+          <slot></slot>
+        </main>
+        <footer>
+          <p>Footer content goes here</p>
+        </footer>
+      </div>
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'wrapper': Wrapper
+  }
+}
